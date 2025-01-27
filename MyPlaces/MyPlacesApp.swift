8e66c9aa38116627c51b8e7c5b5e5c8d9ed752ac
jonglr <johannes.guler@uzh.ch>
2025-01-27 11:11:13 +0100

    Initial Commit

---
+++ MyPlaces/MyPlacesApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyPlacesApp.swift
+//  MyPlaces
+//
+//  Created by Jon Guler on 27.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyPlacesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MyPlacesTests/MyPlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlacesTests.swift
+//  MyPlacesTests
+//
+//  Created by Jon Guler on 27.01.2025.
+//
+
+import Testing
+@testable import MyPlaces
+
+struct MyPlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
