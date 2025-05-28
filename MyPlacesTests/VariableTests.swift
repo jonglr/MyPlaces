5430417d36c6acb6bcc46eefb22ca58a79a54ef7
jonglr <johannes.guler@uzh.ch>
2025-05-28 08:39:20 +0200

    Added TestCases

--- MyPlacesTests/VariableTests.swift
+++ MyPlacesTests/VariableTests.swift
@@ -1,5 +1,5 @@
 //
-//  MyPlacesTests.swift
+//  VariableTests.swift
 //  MyPlacesTests
 //
 //  Created by Jon Guler on 27.01.2025.
@@ -8,10 +8,16 @@
 import XCTest
 @testable import MyPlaces
 
-final class MyPlacesTests: XCTestCase {
+final class VariablesTests: XCTestCase {
     
-    override func fclassConversion() throws {
-        
+    func testFClassConversion() {
+        // Arrange (Given)
+        let flcass = "airfield"
+        let variableManager = VariableManager()
+        // Act (When)
+        let convFclass = variableManager.fclassConversion(fclass: flcass)
+        // Assert (Then)
+        XCTAssertEqual(convFclass, 0.0)
     }
     
 }
