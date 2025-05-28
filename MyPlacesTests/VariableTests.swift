be45f30b395162fca08ee2affa9e2327d654d283
jonglr <johannes.guler@uzh.ch>
2025-05-28 08:38:58 +0200

    Added first Test Case

---
+++ MyPlacesTests/VariableTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlacesTests.swift
+//  MyPlacesTests
+//
+//  Created by Jon Guler on 27.01.2025.
+//
+
+import XCTest
+@testable import MyPlaces
+
+final class MyPlacesTests: XCTestCase {
+    
+    override func fclassConversion() throws {
+        
+    }
+    
+}
